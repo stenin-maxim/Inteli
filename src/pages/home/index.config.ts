b9ad5f63bcacacd60770d25c39771c02371023b4
Maxim <stenin.dev@gmail.com>
2024-03-01 08:56:11 +0300

    Created page home

--- src/pages/home/index.config.ts
+++ src/pages/home/index.config.ts
@@ -2,7 +2,7 @@ export default {
   backgroundColor: '@bgColor',
   navigationBarBackgroundColor: '@navBgColor',
   navigationBarTextStyle: '@navTxtStyle',
-  navigationBarTitleText: 'Home',
-  navigationStyle: 'custom',
+  // navigationBarTitleText: 'Home',
+  // navigationStyle: 'custom',
   disableScroll: true,
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,29 +1,298 @@
 import React from 'react';
-import { Text, View } from '@ray-js/ray';
-import { TopBar } from '@/components';
-import { IconFont } from '@/components/icon-font';
+import { View, Button, Icon, Text } from '@ray-js/ray';
+import { navigateTo, vibrateShort, showToast, showModal } from '@ray-js/ray';
 import styles from './index.module.less';
-import { useActions} from '@ray-js/panel-sdk';
+import { useActions, useProps, useDevInfo } from '@ray-js/panel-sdk';
+import Strings from '../../i18n';
 
 export function Home() {
-  const actions = useActions();
+    const ACTIONS: any = useActions();
+    let alarm: boolean = useProps((props): boolean => Boolean(props.alarm));
+    let craneCondition1: boolean = useProps((props): boolean => Boolean(props.switch));
+    let craneCondition2: boolean = useProps((props): boolean => Boolean(props.switch_2));
+    let cleaning: boolean = useProps((props): boolean => Boolean(props.cleaning));
+    let multizoneMode: boolean = useProps((props): boolean => Boolean(props.multizone_mode));
+    
+    let sensorsLeak = [];
+    let sensorsSecurityMode = [];
+    let textDevice: string = Strings.getLang('device'),
+        textAlarm: string = Strings.getLang('text_alarm'),
+        textNotify: string = Strings.getLang('notify'),
+        textLowBatteryOrSignal: string = Strings.getLang('low_battery_or_signal'),
+        textDisableAlarm: string = Strings.getLang('disable_alarm'),
+        textSwitchOn: string = Strings.getLang('switch_on'),
+        textSwitchOff: string = Strings.getLang('switch_off'),
+        textNotifyCleaning: string = Strings.getLang('text_notify_cleaning'),
+        textCleaningModeOn: string = Strings.getLang('text_cleaning_mode_on'),
+        textCleaningModeOff: string = Strings.getLang('text_cleaning_mode_off'),
+        textButtonCleaning: string = Strings.getLang('text_cleaning'),
+        textSensors: string = Strings.getLang('sensors'),
+        textCancel: string = Strings.getLang('cancel'),
+        textConfirm: string = Strings.getLang('confirm'),
+        textContentAlarm: string = Strings.getLang('text_content_alarm'),
+        textSettings: string = Strings.getLang('settings'),
+        textJournal: string = Strings.getLang('journal');
 
-  return (
-    <View className={styles.view}>
-      <TopBar />
-      <View
-        className={styles.content}
-        onClick={() => {
-          actions.switch_1.toggle();
-        }}
-      >
-        <View className={styles['space-between']} style={{ marginTop: '50rpx' }}>
-          <Text>Public SDM Template</Text>
-          <IconFont icon="sun" />
+    /**
+     * Кнопка выключения аварии
+     * 
+     * @returns object|false
+     */
+    function alarmResetButton(): object|false
+    {
+        if (alarm) {
+            return (
+                <View className={styles.blockAlarm}>
+                    <View className={styles.alarmButton}
+                        onClick={() => { showModal(confirm()) }}
+                    >
+                        <Icon type="icon-cancel" size={35} color="red"></Icon>
+                        <Text>{textDisableAlarm}</Text>
+                    </View>
+                </View>
+            );
+        }
+
+        return false;
+    }
+
+    /**
+     * Параметры модального окна при снятии аварии
+     * 
+     * @returns object
+     */
+    function confirm(): object
+    {
+        return {
+            title: textDisableAlarm,
+            content: textContentAlarm,
+            cancelText: textCancel,
+            confirmText: textConfirm,
+            confirmColor: '#ff0000',
+            success: (param: any): void => {
+                if (param.confirm) {
+                    ACTIONS.alarm.off(); 
+                    sensorsLeak = []; 
+                    sensorsSecurityMode = [];
+                }
+            },
+        }
+    }
+
+    /**
+     * Уведомление при низкой батареи или сигнала
+     */
+    function notifyLowBatteryOrSignal(): object
+    {
+        if (alarm && (sensorsSecurityMode.length > 0)) {
+            return notify(textLowBatteryOrSignal + ' ' + textSensors + ': ' + sensorsLeak.join(', '));
+        }
+    }
+
+    /**
+     * Уведлмление при протечки на определенных датчиках
+     */
+    function notifyLeak(): object
+    {
+        if (alarm && (sensorsLeak.length > 0)) {
+            return notify(textAlarm + ' ' + textSensors + ': ' + sensorsLeak.join(', '));
+        }
+    }
+
+    /**
+     * Уведомление о включении уборки
+     * 
+     * @returns object|false
+     */
+    function notifyCleaning(): object|false
+    {
+        if (cleaning) {
+            return notify(textNotifyCleaning);
+        }
+
+        return false;
+    }
+
+    /**
+     * Уведомление о статусе устройства в сети
+     * 
+     * @returns object
+     */
+    function notifyDevice(): object
+    {
+        if (!useDevInfo().isOnline) {
+            return notify(textDevice);
+        }
+    }
+
+    /**
+     * Каркас уведомления
+     * 
+     * @param text string
+     * @returns object
+     */
+    function notify(text: string): object
+    {
+        return (
+            <View className={styles.blockNotify}>
+                <Text className={styles.textLeft}>{text}</Text>
+                <Text className={styles.textRight}>{textNotify}</Text>
+            </View>
+        )
+    }
+
+    /**
+     * Вкл/выкл уборку
+     */
+    function startStopCleaning(): void
+    {
+        let text: string;
+
+        if (!cleaning) {
+            text = textCleaningModeOn;
+            ACTIONS.cleaning.on();
+        } else {
+            text = textCleaningModeOff;
+            ACTIONS.cleaning.off();
+        }
+
+        showToast({
+            title: text,
+            duration: 4000,
+        })
+    }
+
+    /**
+     * Состояние цвета иконки при вкл/выкл уборки
+     * 
+     * @returns object
+     */
+    function colorIconCleaning(): object {
+        let color: string = '#00BFFF';
+        
+        if (cleaning) {
+            color = 'orange';
+        }
+
+        return (
+            <Icon type="icon-a-dropfill" size={40} color={color}/>
+        )
+    }
+
+    function clickCraneCondition(dpSwitch: string): void
+    {
+        if (!alarm) {
+            ACTIONS[dpSwitch].toggle();
+        }
+    }
+
+    function blockCraneCondition(craneCondition: boolean): object
+    {
+        if (craneCondition) {
+            return (
+                <View>
+                    <View className={styles.openClose}>
+                        <Text className={styles.textSwitch}>{textSwitchOn}</Text>
+                    </View>
+                    <View className={styles.waves}>
+                        <View className={styles.wave1}></View>
+                        <View className={styles.wave2}></View>
+                    </View>
+                </View>
+            )
+        }
+
+        return (
+            <View>
+                <View className={styles.openClose}>
+                    <Text className={styles.textSwitch}>{textSwitchOff}</Text>
+                </View>
+                <View className={styles.waves}>
+                    <View className={styles.wave3}></View>
+                    <View className={styles.wave4}></View>
+                </View>
+            </View>
+        )
+    }
+
+    function enableDisableZone()
+    {
+        if (multizoneMode) {
+            return (
+                <React.Fragment>
+                    <View className={styles.zone}
+                        onClick={() => { clickCraneCondition('switch'); vibrateShort({type: 'heavy'}); vibrateShort({type: 'heavy'});
+                    }}>
+                        <Text>Zone 1</Text>
+                        {blockCraneCondition(craneCondition1)}
+                    </View>
+                    <View className={styles.zone}
+                        onClick={() => { clickCraneCondition('switch_2'); vibrateShort({type: 'heavy'}); vibrateShort({type: 'heavy'});
+                    }}>
+                        <Text>Zone 2</Text>
+                        {blockCraneCondition(craneCondition2)}
+                    </View>
+                </React.Fragment>
+            )
+        }
+        return (
+            <React.Fragment>
+                <View onClick={() => { clickCraneCondition('switch'); vibrateShort({type: 'heavy'}); vibrateShort({type: 'heavy'});}}>
+                    {blockCraneCondition(craneCondition1)}
+                </View>
+            </React.Fragment>
+        )
+    }
+
+    return (
+        <View className={styles.view}>
+            <View className={styles.logo}>
+                <Text className={styles.logoText}>{useDevInfo().name}</Text>
+            </View>
+            <View>
+                {notifyDevice()}
+                {notifyLeak()}
+                {notifyLowBatteryOrSignal()}
+                {notifyCleaning()}
+                {alarmResetButton()}
+            </View>
+            <View className={styles.blockCraneCondition}>
+                {enableDisableZone()}
+            </View>
+            <View className={styles.blockFooter}>
+                <View className={styles.navButtons}>
+                    <Button
+                        className={styles.button}
+                        onClick={() => { startStopCleaning(); vibrateShort({type: 'heavy'}); vibrateShort({type: 'heavy'}); }}
+                    >
+                        { colorIconCleaning() }
+                        <Text className={styles.textButton}>{textButtonCleaning}</Text>
+                    </Button>
+                    <Button
+                        className={styles.button}
+                        onClick={() => navigateTo({ url: '/pages/journal/index'})}
+                    >
+                        <Icon type="icon-a-scrollfill" size={40}/>
+                        <Text className={styles.textButton}>{textJournal}</Text>
+                    </Button>
+                    <Button
+                        className={styles.button}
+                        onClick={() => navigateTo({ url: '/pages/sensors/index'})}
+                    >
+                        <Icon type="icon-a-dotradiowavesleftandright" size={40}/>
+                        <Text className={styles.textButton}>{textSensors}</Text>
+                    </Button>
+                    <Button
+                        className={styles.button}
+                        onClick={() => navigateTo({ url: '/pages/settings/index'})}
+                    >
+                        <Icon type="icon-a-wrenchandscrewdriverfill" size={40}/>
+                        <Text className={styles.textButton}>{textSettings}</Text>
+                    </Button>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 export default Home;
