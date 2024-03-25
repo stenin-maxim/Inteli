187652c95d96b77869ad4dade2ac3ee8f764b82e
Stenin Maxim <stenin.dev@gmail.com>
2024-03-25 15:55:37 +0300

    Refactor lang

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -8,10 +8,10 @@ import { useActions, useProps } from '@ray-js/panel-sdk';
 export default () => {
     const ACTIONS: any = useActions();
     let multizoneMode: boolean = useProps((props): boolean => Boolean(props.multizone_mode));
-    let textCounterSettings: string = Strings.getLang('counterSettings'),
-        textRadioSensorSettings: string = Strings.getLang('radioSensorSettings'),
-        textWiredSensorSettings: string = Strings.getLang('wiredSensorSettings'),
-        textMultizoneMode: string = Strings.getLang('multizoneMode');
+    let textCounterSettings: string = Strings.getLang('counter_settings'),
+        textRadioSensorSettings: string = Strings.getLang('radio_sensor_settings'),
+        textWiredSensorSettings: string = Strings.getLang('wired_sensor_settings'),
+        textMultizoneMode: string = Strings.getLang('multizone_mode');
 
     function multizoneModeSwitch(): void
     {
@@ -40,13 +40,13 @@ export default () => {
                 <Text className={styles.linkText}>{textRadioSensorSettings}</Text>
                 <Icon type="icon-right" size={18}></Icon>
             </View>
-            <View className={styles.link} 
+            {/* <View className={styles.link} 
                 onClick={() => navigateTo({ url: '/pages/counters/index'})}
             >
                 <Icon type="icon-timer" size={30} color="#00BFFF" style={{ position: 'relative', top: '2px' }}></Icon>
                 <Text className={styles.linkText}>{textCounterSettings}</Text>
                 <Icon type="icon-right" size={18}></Icon>
-            </View>
+            </View> */}
             <View className={styles.info} 
                 onClick={() => navigateTo({ url: '/pages/info/index'})}
             >
