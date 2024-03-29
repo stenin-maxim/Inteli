24fb8c7be6f6ac5d33cfa39af22a117332267045
Stenin Maxim <stenin.dev@gmail.com>
2024-03-29 16:48:38 +0300

    Refactor page counters

--- src/pages/counters/index.tsx
+++ src/pages/counters/index.tsx
@@ -4,12 +4,12 @@ import styles from './index.module.less';
 import { navigateTo } from '@ray-js/ray';
 import Strings from '../../i18n';
 import { useProps } from '@ray-js/panel-sdk';
+import {indicatorCounter} from '@/components/counter/indicator-counter';
 
 export default () => {
     let textCounter: string = Strings.getLang('counter'),
         textMeter: string = Strings.getLang('meter');
-    let counterName: string = useProps((props): string => String(props.counter_name));
-    let arr = counterName.split(';');
+    let counterName: Array<string> = useProps((props): string => String(props.counter_name)).split(';');
     let counter_1: number = useProps((props): number => Number(props.counter_1)),
         counter_2: number = useProps((props): number => Number(props.counter_2)),
         counter_3: number = useProps((props): number => Number(props.counter_3)),
@@ -19,40 +19,13 @@ export default () => {
         multiplier_3: string = useProps((props): string => String(props.multiplier_3)),
         multiplier_4: string = useProps((props): string => String(props.multiplier_4));
 
-    /**
-     * Показатель счетчика
-     * 
-     * @param counter - показатель счетчика
-     * @param multiplier - импульс счетчика
-     * @returns 
-     */
-    function indicatorCounter(counter: number, multiplier: string): string
-    {
-        let counterMultiplier = counter * Number(multiplier);
-        let arr: string[] = String(counterMultiplier).split('');
-
-        if (arr.length - 1 < 3) {
-            for (let i = arr.length - 1; arr.length <= 3; i++) {
-                arr.unshift('0');
-            }
-        }
-
-        if (arr.length > 8) {
-            arr.splice(0, 1);
-        }
-
-        arr.splice(-3, 0, '.');
-
-        return arr.join('');
-    }
-
     return (
         <View>
             <View className={styles.counter} onClick={() => navigateTo({ url: '/pages/counter-1/index'})}>
                 <View className={styles.counterBlock}>
                     <Icon type="icon-timer" size={30} color="#00BFFF" style={{ position: 'relative', top: '2px' }}></Icon>
                     <View className={styles.counterNameText}>
-                        <Text>{arr[0]}</Text>
+                        <Text>{counterName[0]}</Text>
                         <Text className={styles.counterText}>{textCounter + ' №1'}</Text>
                     </View>
                 </View>
@@ -66,7 +39,7 @@ export default () => {
                 <View className={styles.counterBlock}>
                     <Icon type="icon-timer" size={30} color="#00BFFF" style={{ position: 'relative', top: '2px' }}></Icon>
                     <View className={styles.counterNameText}>
-                        <Text>{arr[1]}</Text>
+                        <Text>{counterName[1]}</Text>
                         <Text className={styles.counterText}>{textCounter + ' №2'}</Text>
                     </View>
                 </View>
@@ -80,7 +53,7 @@ export default () => {
                 <View className={styles.counterBlock}>
                     <Icon type="icon-timer" size={30} color="#00BFFF" style={{ position: 'relative', top: '2px' }}></Icon>
                     <View className={styles.counterNameText}>
-                        <Text>{arr[2]}</Text>
+                        <Text>{counterName[2]}</Text>
                         <Text className={styles.counterText}>{textCounter + ' №3'}</Text>
                     </View>
                 </View>
@@ -94,7 +67,7 @@ export default () => {
                 <View className={styles.counterBlock}>
                     <Icon type="icon-timer" size={30} color="#00BFFF" style={{ position: 'relative', top: '2px' }}></Icon>
                     <View className={styles.counterNameText}>
-                        <Text>{arr[3]}</Text>
+                        <Text>{counterName[3]}</Text>
                         <Text className={styles.counterText}>{textCounter + ' №4'}</Text>
                     </View>
                 </View>
