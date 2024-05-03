f7de0d73edc368689495c297c74d93df34762662
Stenin Maxim <stenin.dev@gmail.com>
2024-05-03 11:11:30 +0300

    Add const COUNTER_NAMES

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,4 @@
+const COUNTER_NAMES = 'Counter 1;Counter 2;Counter 3;Counter 4';
 const WIRED_SENSOR_NAMES_1 = 'Wired sensor 1;Wired sensor 2;Wired sensor 3;Wired sensor 4;Wired sensor 5;Wired sensor 6';
 const WIRED_SENSOR_NAMES_2 = 'Wired sensor 7;Wired sensor 8;Wired sensor 9;Wired sensor 10;Wired sensor 11;Wired sensor 12';
 const RADIO_SENSOR_NAMES_1 = 'Radio sensor 1;Radio sensor 2;Radio sensor 3;Radio sensor 4;Radio sensor 5;Radio sensor 6;Radio sensor 7;Radio sensor 8';
@@ -5,4 +6,4 @@ const RADIO_SENSOR_NAMES_2 = 'Radio sensor 9;Radio sensor 10;Radio sensor 11;Rad
 const RADIO_SENSOR_NAMES_3 = 'Radio sensor 17;Radio sensor 18;Radio sensor 19;Radio sensor 20;Radio sensor 21;Radio sensor 22;Radio sensor 23;Radio sensor 24';
 const RADIO_SENSOR_NAMES_4 = 'Radio sensor 25;Radio sensor 26;Radio sensor 27;Radio sensor 28;Radio sensor 29;Radio sensor 30;Radio sensor 31;Radio sensor 32';
 
-export {WIRED_SENSOR_NAMES_1, WIRED_SENSOR_NAMES_2, RADIO_SENSOR_NAMES_1, RADIO_SENSOR_NAMES_2, RADIO_SENSOR_NAMES_3, RADIO_SENSOR_NAMES_4};
+export {COUNTER_NAMES, WIRED_SENSOR_NAMES_1, WIRED_SENSOR_NAMES_2, RADIO_SENSOR_NAMES_1, RADIO_SENSOR_NAMES_2, RADIO_SENSOR_NAMES_3, RADIO_SENSOR_NAMES_4};
